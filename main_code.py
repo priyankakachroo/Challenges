67724d95b7d7d52915f143803baf4a0173b04e3b
MicSeit <mic.seit@gmail.com>
2021-04-20 00:04:22 +0300

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -1,5 +1,17 @@
+# Create a class object that store the remaing value and the step we are in
+class node:
+  def __init__(self, value, step):
+    self.value = value
+    self.step = step
+ 
+# Returns minimum number of operations
 def min_operations(x, y):
-  # Fill this in.
+  # A variable to keep track of visited numbers.
+  visited = set()
+
+
+
+
 
 print(min_operations(6, 20))
 # (((6 - 1) * 2) * 2) = 20 : 3 operations needed only
