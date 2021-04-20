f933ecc95bf0445b8bbab8f7d4c38061abfc6ad9
plagiagia <dimitris.giannoul@gmail.com>
2021-04-20 16:51:16 +0200

    edit readme

--- main_code.py
+++ main_code.py
@@ -1,19 +1,16 @@
 # Create a class object that store the remaing value and the step we are in
 class node:
-  def __init__(self, value, step):
-    self.value = value
-    self.step = step
- 
+    def __init__(self, values: list):
+        self.start = values[0]
+        self.end = values[1]
+
+
 # Returns minimum number of operations
 def min_operations(x, y):
-  # A variable to keep track of visited numbers.
-  visited = set()
-
-
-
+    # A variable to keep track of visited numbers.
+    visited = set()
 
 
 print(min_operations(6, 20))
 # (((6 - 1) * 2) * 2) = 20 : 3 operations needed only
 #
-
