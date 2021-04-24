9485ec0dbe43a54f1e4c39da68c52f1754a1fb36
plagiagia <dimitris.giannoul@gmail.com>
2021-04-24 20:55:37 +0200

    fix runtime problem

--- Operations.py
+++ Operations.py
@@ -1,6 +1,3 @@
-from math import gcd
-
-
 class Operations:
     """
   A class that calculates the operations needed to reach from a specific number to another,
@@ -16,21 +13,16 @@ class Operations:
     results: A list to save the results of the operations and look for the n2
     next_step: A temporary list to save the next branch of operations
     """
-        self.gcd = gcd(n1, n2)
         self.n1 = n1
         self.n2 = n2
-        self.n1_simple = self.n1 / self.gcd
-        self.n2_simple = self.n2 / self.gcd
         self.operations = 0
-        self.results = [self.n1_simple]
-        self.next_step = []
 
-    def reached(self):
+    def reached(self, num):
         """
     Checks if we reach the number we have as target
     :return: -1 if we reached the target, else 0
     """
-        if self.n2_simple in self.results:
+        if self.n1 == num:
             return -1
         else:
             return 0
@@ -40,34 +32,28 @@ class Operations:
     :param num: An integer number
     :return: A list with the results of the operations on num
     """
-        subtract = num - 1
-        mul = num * 2
-        temp = []
+        addition = num + 1
+        division = num / 2
 
-        if 0 <= subtract and subtract not in self.results:
-            # Don't save in the list numbers smaller than 0
-            temp.append(subtract)
-        if mul not in self.results:
-            temp.append(mul)
-
-        return temp
+        # if end number is even and bigger than start the divide by 2
+        if num % 2 == 0 and self.n1 < num:
+            return division
+        # if end number is even and bigger than start then add 1
+        elif num % 2 != 0 and self.n1 < num:
+            return addition
+        # all other cases return the addition
+        else:
+            return addition
 
     def calculate_operations(self):
         """
-    Here we calculate the number of operations needed to reach the target number.
-    :return: The number of operations
-    """
-        while self.reached() == 0:
+        Here we calculate the number of operations needed to reach the target number.
+        :return: The number of operations
+        """
+        num = self.n2
+        while self.reached(num) == 0:
             self.operations += 1
+            result = self.step(num)
+            num = result
 
-            for each in self.results:
-                # for each number in results
-                for i in self.step(each):
-                    # for each result from the previous numbers
-                    # calculate the next step and append it to the list
-                    self.next_step.append(i)
-            # Change the lists
-            self.results = self.next_step
-            # Reset the temp list
-            self.next_step = []
         return self.operations
--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-import matplotlib.pyplot as plt
-
 from Operations import *
 
-a = Operations(500, 100000)
-print(a.gcd)
+a = Operations(416, 465635)
 calc = a.calculate_operations()
 print(f"Number of calculations to go from {a.n1} to {a.n2} are {calc}")
 
-
-# test_list = [x for x in range(1, 1000)]
-# calcs = []
-#
-# for each in test_list:
-#     a = Operations(1, each)
-#     calcs.append(a.calculate_operations())
-
-# plt.plot(test_list, calcs)
-# plt.grid()
-# plt.show()
-
-# print(max(calcs))
